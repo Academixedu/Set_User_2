de589a577b126272446eb8060bcc5e7e83bdac63
saipradeep484 <saipradeep484@gmail.com>
2024-08-05 16:56:58 +0530

    dsjl

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,15 @@
-public class BasedOnName{
+import java.util.Comparator;
+
+public class BasedOnName implements Comparator<Person>{
+
+  @Override
+  public int compare(Person o1, Person o2) {
+    // TODO Auto-generated method stub
+   return o1.getSets().compareTo(o2.getSets());
+  }
+
+  
   // implements comparator here and make person class as Target <>
+
 // Prepare a Code for Sorting Order BasedOnName 
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,33 @@
-public class Person{
-// prepare a Person class here and  implmenet Getters and Setters,constructor,tostring,Comparable
+
+public class Person implements Comparable<Person> {
+    private String sets;
+   
+
+    public String getSets() {
+        return sets;
+    }
+
+    public Person() {
+    }
+
+    public void setSets(String sets) {
+        this.sets = sets;
+    }
+
+    public Person(String sets) {
+        this.sets = sets;
+       
+    }
+
+    @Override
+    public String toString() {
+        return "Person [sets=" + sets  + "]";
+    }
+    public int compareTo(Person pr)
+
+    {
+        return 0;
+    }
+    // prepare a Person class here and implmenet Getters and
+    // Setters,constructor,tostring,Comparable
 }
