0943deddee6c929abd5ba8afeba29f922efd61b3
ranjith81869 <ranjith81869@email.com>
2024-08-05 13:00:26 +0530

    Set_User_2

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,34 @@
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class BasedOnName{
   // implements comparator here and make person class as Target <>
 // Prepare a Code for Sorting Order BasedOnName 
+public static void main(String[] args) {
+  Set<Person> list = new TreeSet<>();
+  list.add(new Person(0, "Ranji", 0, null));
+  list.add(new Person(0, "Sakshi", 0, null));
+  list.add(new Person(0, "Raj", 0, null));
+  list.add(new Person(0, "Vishal", 0, null));
+  for (Person p : list) {
+    System.out.println(p);
+  }
+  Comparator<Person> p=null;
+Scanner in=new Scanner(System.in);
+System.out.println("1. Sort By presonname");
+System.out.println("2. Sort By personid");
+int choice=in.nextInt();
+if(choice==1){
+
+p=(o1,o2)->o1.getPersonname().compareTo(o2.getPersonname());
+}
+else if(choice==2){
+
+p=(o1,o2)->o1.getPersonid().compareTo(o2.getPersonid());
+    
+}
+list.stream().sorted(p).forEach(System.out::println);
+}
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,129 @@
 public class Person{
 // prepare a Person class here and  implmenet Getters and Setters,constructor,tostring,Comparable
+public Person(int i, String string, int j, String string2) {
+    
 }
+
+public static final int pid = 0;
+public Object personname;
+public double personsal;
+public Object persondesg;
+
+public class person implements Comparable<Person>{
+    private int personid;
+    private String personname;
+    private double personsal;
+    private String persondesg;
+  
+    @Override
+    public int compareTo(Person o) {
+         return 0;
+    }
+
+    public int getPersonid() {
+        return personid;
+    }
+
+    public void setPersonid(int personid) {
+        this.personid = pid;
+    }
+
+    public String getPersonname() {
+        return personname;
+    }
+
+    public void setPersonname(String personname) {
+        this.personname = personname;
+    }
+
+    public double getPersonsal() {
+        return personsal;
+    }
+
+    public void setPersonsal(double personsal) {
+        this.personsal = personsal;
+    }
+
+    public String getPersondesg() {
+        return persondesg;
+    }
+
+    public void setPersondesg(String persondesg) {
+        this.persondesg = persondesg;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [personid=" + personid + ", personname=" + personname + ", personsal=" + personsal + ", persondesg=" + persondesg + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + personid;
+        result = prime * result + ((personname == null) ? 0 : personname.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(personsal);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((persondesg == null) ? 0 : persondesg.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (pid != other.pid)
+            return false;
+        if (personname == null) {
+            if (other.personname != null)
+                return false;
+        } else if (!personname.equals(other.personname))
+            return false;
+        if (Double.doubleToLongBits(personsal) != Double.doubleToLongBits(other.personsal))
+            return false;
+        if (persondesg == null) {
+            if (other.persondesg != null)
+                return false;
+        } else if (!persondesg.equals(other.persondesg))
+            return false;
+        return true;
+    }
+
+    public person(int pid, String pname, double psal, String pdesg) {
+        this.personid = pid;
+        this.personname = pname;
+        this.personsal = psal;
+        this.persondesg = pdesg;
+    }
+
+    public person() {
+    }
+    
+    
+    
+}
+
+public String getPersonname() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getPersonname'");
+}
+
+public String getPersonid() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getPersonid'");
+}
+
+}
+
+    
+
+    
+
+
