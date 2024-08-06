5cdfa8bc7b28449c6a979ac50b9396b729fb88e1
Your Name <your@email.com>
2024-08-06 19:32:47 +0530

    Set_User_2 done

--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,15 @@
-public class TreeSets{
-// prepare an Object for TreeSet and Load Person Class Objects in it 
-  // Make the Sorting Order Based on Comparator
+import java.util.TreeSet;
+
+public class TreeSets {
+    public static void main(String[] args) {
+        TreeSet<Person> treeSet = new TreeSet<>(new BasedOnName());
+
+        treeSet.add(new Person(1, "Alice", 50000, "Developer"));
+        treeSet.add(new Person(2, "Bob", 60000, "Manager"));
+        treeSet.add(new Person(3, "Charlie", 55000, "Analyst"));
+
+        for (Person person : treeSet) {
+            System.out.println(person);
+        }
+    }
 }
