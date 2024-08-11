fd3b70d7f7cbfc68f09b6452c0629fbca653ea5e
swapna-321 <swapna@gmail.com>
2024-08-11 15:34:56 +0530

    sets

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,15 @@
-public class BasedOnName{
+
+  import java.util.Comparator;
+public class BasedOnName implements Comparator<Person>{
   // implements comparator here and make person class as Target <>
 // Prepare a Code for Sorting Order BasedOnName 
-}
+
+     @Override
+
+     public int compare(Person p1,Person p2){
+
+        return p1.getPname().compareTo(p2.getPname());
+     }
+    }
+
+  
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,26 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 public class TreeSets{
-// prepare an Object for TreeSet and Load Person Class Objects in it 
-  // Make the Sorting Order Based on Comparator
+  // prepare an Object for TreeSet and Load Person Class Objects in it 
+    // Make the Sorting Order Based on Comparator
+  
+  public static void main(String[] args){
+  
+    Comparator<Person> pp = new BasedOnName();
+
+    Set <Person> p =new TreeSet<>(pp);
+   
+   p.add(new Person(1,"soppi", 300.0, "doctor"));
+   p.add(new Person(1,"chinnu", 200.0, "Beautician"));
+   p.add(new Person(2, "Tony", 4500.00, "lawyer"));
+   p.add(new Person(3, "Pavi", 100.50, "Engineer"));
+  
+   for(Person x:p){
+    System.out.println(x);
+   }
+  }
 }
+  
+
+
