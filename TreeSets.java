b54f979bbf16f697741dc3ae27b32cc0424eab8d
swapna-321 <swapna@gmail.com>
2024-08-11 15:35:14 +0530

    sets

--- TreeSets.java
+++ TreeSets.java
@@ -11,10 +11,10 @@ public class TreeSets{
 
     Set <Person> p =new TreeSet<>(pp);
    
-   p.add(new Person(1,"soppi", 300.0, "doctor"));
-   p.add(new Person(1,"chinnu", 200.0, "Beautician"));
-   p.add(new Person(2, "Tony", 4500.00, "lawyer"));
-   p.add(new Person(3, "Pavi", 100.50, "Engineer"));
+   p.add(new Person(1,"soppi", 3300.0, "doctor"));
+   p.add(new Person(1,"chinnu", 1200.0, "Beautician"));
+   p.add(new Person(2, "Tony", 5000.00, "lawyer"));
+   p.add(new Person(3, "Pavi", 4500.50, "Engineer"));
   
    for(Person x:p){
     System.out.println(x);
