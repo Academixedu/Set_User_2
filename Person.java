e616da958dc04439f3131a4e4d478868ace00ac9
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-13 20:05:00 +0530

    Swarupa

--- Person.java
+++
@@ -1,3 +0,0 @@
-public class Person{
-// prepare a Person class here and  implmenet Getters and Setters,constructor,tostring,Comparable
-}
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,19 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
+public static void main(String[] args) {
+  
+TreeSet<Person1> t1=new TreeSet<>(new BasedOnName());
+t1.add(new Person1("Swarupa", 21));
+t1.add(new Person1("Sujatha", 22));
   // Make the Sorting Order Based on Comparator
+  Iterator<Person1> a1=t1.iterator();
+  while(a1.hasNext()){
+    System.out.println(a1.next());
+
+  }
+
+}
 }
