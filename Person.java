eab6c4d8e6fe0e2ef8bcdf585fa7cb104d6dfee4
psvsai <pasupuletisvsm245@gmail.com>
2024-08-13 20:32:25 +0530

    PSVSAI

--- Person.java
+++ Person.java
@@ -1,3 +1,51 @@
 public class Person{
 // prepare a Person class here and  implmenet Getters and Setters,constructor,tostring,Comparable
+    private String pname;
+    private int page;
+    private String pdesg;
+    private double psal;
+    public Person(String pname, int page, String pdesg, double psal) {
+        this.pname = pname;
+        this.page = page;
+        this.pdesg = pdesg;
+        this.psal = psal;
+    }
+   
+    public String getPname() {
+        return pname;
+    }
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        this.page = page;
+    }
+    public String getPdesg() {
+        return pdesg;
+    }
+    public void setPdesg(String pdesg) {
+        this.pdesg = pdesg;
+    }
+    public double getPsal() {
+        return psal;
+    }
+    public void setPsal(double psal) {
+        this.psal = psal;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [pname=" + pname + ", page=" + page + ", pdesg=" + pdesg + ", psal=" + psal + "]";
+    }
+
+   
+
+    @Override
+    public boolean equals(Object obj) {
+       
+        return super.equals(obj);
+    }
 }
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,15 @@
+import java.util.TreeSet;
+
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
+  public static void main(String[] args) {
+    TreeSet<Person> p = new TreeSet<>();
+    p.add(new Person("PSVSAI", 20, "ASSOCIATE", 18000));
+    p.add(new Person("MAHESH", 24, "HR", 35000));
+    p.add(new Person("VENKATA", 23, "TL", 25550));
+    for (Person per : p) {
+      System.out.println(per);
+    }
+  }
 }
