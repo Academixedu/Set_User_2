6e45efd320d50ba7821ad1e36fe018fd5ca3340c
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-08-13 19:29:13 +0530

    set

--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,14 @@
+import java.util.TreeSet;
+
 public class TreeSets{
+  public static void main(String args[]){
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
+  TreeSet<Person> t=new TreeSet<>();
+  t.add(new Person(1, "sanky",21));
+t.add(new Person(4,"sri",22));
+t.add(new Person(2, "Ram", 34));
+for (Person i : t) {
+    System.out.println(i);
 }
+      }}
