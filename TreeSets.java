eb29fdcfadb8489d4f11f999e8802773b7a61c1e
Shreyash000007 <shreyasharg@gmail.com>
2024-08-13 18:11:08 +0530

    Shreyash

--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,23 @@
+import java.util.*;
+
+
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
+
+  public static void main(String[] args) 
+  {
+     Comparator <Person> c=new BasedOnName();
+     Set <Person> s=new TreeSet<>(c)  ;
+    
+     
+     s.add(new Person("Shreyash", 22, 9850));
+     s.add(new Person("Abhi", 21, 98));
+     s.add(new Person("Ratan", 28, 738));
+      for (Person person : s) 
+      {
+          System.out.println(person);  
+      }
+  
+  }
 }
