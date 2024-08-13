a43e145b99018cd8122c6b8234b0930a521e4434
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-13 18:20:07 +0530

    from fayaz

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,15 @@
-public class BasedOnName{
+
   // implements comparator here and make person class as Target <>
 // Prepare a Code for Sorting Order BasedOnName 
+import java.util.Comparator;
+
+public class BasedOnName implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().compareTo(p2.getName());
+    }
+    
 }
+
+
