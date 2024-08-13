055cab076da1c0cef7a553e395436566372f5cf7
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-13 19:25:15 +0530

    sarvesh

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,12 @@
-public class BasedOnName{
+import java.util.Comparator;
+
+public class BasedOnName implements Comparator<Person>{
   // implements comparator here and make person class as Target <>
-// Prepare a Code for Sorting Order BasedOnName 
+// Prepare a Code for Sorting Order BasedOnName
+@Override
+public int compare(Person o1,Person o2){
+  return o1.getPname().compareTo(o2.getPname());
+
+}
+
 }
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,30 @@
+import java.util.Comparator;
+import java.util.TreeSet;
+
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
+
+  public static void main(String[] args) {
+    Comparator<Person>c=new BasedOnName();
+
+    TreeSet <Person> t=new TreeSet<>(c);
+
+    t.add(new Person(22, "shreyash", 120000, "Dev"));
+    t.add(new Person(21, "sarvesh", 120000, "Dev"));
+    t.add(new Person(22, "abhi", 120000, "Dev"));
+
+for(Person p:t){
+  System.out.println(p);
+}
+
+
+
+
+  }
+
+
+
+
+
 }
