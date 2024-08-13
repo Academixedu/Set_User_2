f5eb230c27dd2e61450957df85579a404567649c
sujatha-94 <sujathakarri701@gmail.com>
2024-08-13 19:15:58 +0530

    Programs

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,13 @@
-public class BasedOnName{
-  // implements comparator here and make person class as Target <>
+import java.util.Comparator;
+
+public class BasedOnName implements Comparator<Person> {
+ // implements comparator here and make person class as Target <>
 // Prepare a Code for Sorting Order BasedOnName 
+
+  @Override
+  public int compare(Person p1, Person p2){
+    return p1.getName().compareTo(p2.getName());
+  }
+
 }
+
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,18 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
-}
+  public static void main(String[] args) {
+    TreeSet<Person> p = new TreeSet<>(new BasedOnName());
+    p.add(new Person("sujatha",21,21234,"A"));
+    p.add(new Person("swarupa",21,230034,"B"));
+    p.add(new Person("kavitha",21,22345,"C"));
+      Iterator<Person> i=p.iterator();
+      while(i.hasNext()){
+        System.out.println(i.next());
+      }
+    }
+  }
+
