3973a60fcc47c94f6a3433c58e592ce131942c77
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-30 18:31:46 +0530

    save

--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,47 @@
-public class TreeSets{
-// prepare an Object for TreeSet and Load Person Class Objects in it 
-  // Make the Sorting Order Based on Comparator
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + ")";
+    }
 }
+
+class AgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        return Integer.compare(p1.getAge(), p2.getAge());
+    }
+}
+
+public class TreeSets {
+    public static void main(String[] args) {
+        TreeSet<Person> people = new TreeSet<>(new AgeComparator());
+
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
+}
+
