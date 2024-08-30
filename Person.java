dbc31f438533e7e4afb42a337d43a5b1b373a663
Shoebmirza00 <shoebbaig31@gmail.com>
2024-08-30 19:19:15 +0530

    hard

--- Person.java
+++ Person.java
@@ -1,3 +1,34 @@
-public class Person{
+import java.util.Comparator;
+
+public class Person implements Comparator<Person>{
 // prepare a Person class here and  implmenet Getters and Setters,constructor,tostring,Comparable
+private int num;
+private String name;
+
+public Person(int num, String name) {
+    this.num = num;
+    this.name = name;
+}
+public int getNum() {
+    return num;
+}
+public void setNum(int num) {
+    this.num = num;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+@Override
+public String toString() {
+    return "Person [num=" + num + ", name=" + name + "]";
+}
+@Override
+public int compare(Person o1, Person o2) {
+    return o1.getName().compareTo(o2.getName());
+}
+
+
 }
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,24 @@
-public class TreeSets{
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class TreeSets {
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
+
+  public static void main(String[] args) {
+    
+  
+  TreeSet<Person> t = new TreeSet<>(new BasedOnName());
+  t.add(new Person(22, "shoeb"));
+  t.add(new Person(24, "mirza"));
+  
+
+  
+  for (Person f:t){
+    System.out.println(f);
+  }
 }
+
+  
+}
+
