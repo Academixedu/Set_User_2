4f5a2bb1b4f99c45d3d16ed05251aba6987e74c3
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-08-30 17:51:03 +0530

    shahid

--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,21 @@
+import java.lang.reflect.Constructor;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
+  public static void main(String[] args) {
+    Comparator<Person> n=null;
+    BasedOnName c=new BasedOnName();
+ TreeSet<Person>m=new TreeSet<>(c);
+ 
+  m.add(new Person(1,"same"));
+  m.add(new Person(2,"Aame1"));
+  Iterator<Person> v=m.iterator();
+  while(v.hasNext()){
+    System.out.println(v.next());
+  }
+  
+}
 }
