c12845e25516938d8a3068429aee450fa28790e8
Talari-Lavanya <lavanya62811@gmail.com>
2024-08-30 19:08:36 +0530

    Lavanya

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,29 @@
+import java.util.Comparator;
+import java.util.TreeSet;
+
 public class BasedOnName{
   // implements comparator here and make person class as Target <>
 // Prepare a Code for Sorting Order BasedOnName 
+// Comparator implementation for sorting based on name
+public static void main(String[] args) {
+  
+  Comparator<Person> nameComparator = new Comparator<Person>() {
+      @Override
+      public int compare(Person p1, Person p2) {
+          return p1.getPname().compareTo(p2.getPname());
+      }
+  };
+
+  
+  TreeSet<Person> p = new TreeSet<Person>(nameComparator);
+  p.add(new Person(1, "A", 'M', 987654321));
+  p.add(new Person(3, "D", 'F', 123456789));
+  p.add(new Person(2, "B", 'M', 897654321));
+
+
+  for (Person person : p) {
+      System.out.println(person);
+  }
+}
+
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,52 @@
-public class Person{
+public class Person implements Comparable<Person>{
 // prepare a Person class here and  implmenet Getters and Setters,constructor,tostring,Comparable
+private int pid;
+private String pname;
+private char pgender;
+private double pmobileno;
+
+public int getPid() {
+    return pid;
+}
+public void setPid(int pid) {
+    this.pid = pid;
+}
+public String getPname() {
+    return pname;
+}
+public void setPname(String pname) {
+    this.pname = pname;
+}
+public char getPgender() {
+    return pgender;
+}
+public void setPgender(char pgender) {
+    this.pgender = pgender;
+}
+public double getPmobileno() {
+    return pmobileno;
+}
+public void setPmobileno(double pmobileno) {
+    this.pmobileno = pmobileno;
+}
+
+public Person(int pid, String pname, char pgender, double pmobileno) {
+    this.pid = pid;
+    this.pname = pname;
+    this.pgender = pgender;
+    this.pmobileno = pmobileno;
+}
+
+@Override
+public String toString() {
+    return "Person [pid=" + pid + ", pname=" + pname + ", pgender=" + pgender + ", pmobileno=" + pmobileno + "]";
+}
+
+@Override
+public int compareTo(Person o) {
+    return Integer.compare(this.pid, o.pid);
+}
+
+
+
 }
