80a6329f44d3060f6532ef483fb7ab1b3d407745
racharlabhanu <racharlabhanu143@gmail.com>
2024-08-30 20:06:16 +0530

    add

--- BasedOnName.java
+++ BasedOnName.java
@@ -1,4 +1,19 @@
-public class BasedOnName{
+import java.util.Comparator;
+
+ public class BasedOnName implements Comparator<Person>{
+
+  @Override
+  public int compare(Person o1, Person o2) {
+    
+   return o1.getName().compareTo(o2.getName());
+  }
+
+ }
+
+
+
+
+
   // implements comparator here and make person class as Target <>
 // Prepare a Code for Sorting Order BasedOnName 
-}
+
--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,33 @@
+import java.util.TreeSet;
+
 public class TreeSets{
 // prepare an Object for TreeSet and Load Person Class Objects in it 
   // Make the Sorting Order Based on Comparator
-}
+  
+  
+  public static void main(String[] args) {
+   
+  TreeSet t=new TreeSet<>(new BasedOnName());
+    
+   t.add(new Person(1,"bhanu"));
+   t.add(new Person(2,"mani"));
+
+   for (Object object : t) {
+    System.out.println(object);
+    
+   }
+   
+
+  
+   }
+    
+   }
+
+
+   
+   
+
+
+
+
+  
