432cbf657df8a84e09941d4baa14d79f988e90a1
kandgule-dinesh <dineshkandagule@gmail.com>
2024-08-30 18:00:21 +0530

    change1

---
+++ TreeSets.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+public class TreeSets{
+// prepare an Object for TreeSet and Load Person Class Objects in it 
+  // Make the Sorting Order Based on Comparator
+public static void main(String[] args) {
+  TreeSet<Person> d = new TreeSet<>(new BasedOnName());
+d.add(new Person(25, "rohan"));
+d.add(new Person(30, "dinesh"));
+d.add(new Person(20, "nand kishor"));
+for (Person person : d) {
+  System.out.println(person);
+}
+}
+}
