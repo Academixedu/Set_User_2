eb02cc868d64b6b126e3c9759d5914dc1153d6cc
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-08-30 19:52:25 +0530

    Revathi

--- TreeSets.java
+++ TreeSets.java
@@ -1,4 +1,15 @@
+import java.util.TreeSet;
 public class TreeSets{
-// prepare an Object for TreeSet and Load Person Class Objects in it 
-  // Make the Sorting Order Based on Comparator
+  public static void main(String[] args) {
+    TreeSet<Person> t=new TreeSet<>();
+    t.add(new Person(1,"Revathi",'F',30000));
+    t.add(new Person(1,"Revathi",'F',30000));
+    t.add(new Person(4,"Ramya",'F',20000));
+    t.add(new Person(3,"Prasann",'M',10000));
+    t.add(new Person(2,"Bhavya",'F',15000));
+    t.add(new Person(5,"Kalyan",'F',25000));
+    for(Person p:t){
+      System.out.println(p);
+    }
+  }
 }
